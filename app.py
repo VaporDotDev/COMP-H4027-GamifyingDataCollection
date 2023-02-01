b65929402177de44080395264d10992486d7ae36
Dominic Kuncik <itsdomhd@gmail.com>
2023-02-01 10:48:26 +0000

    Added basic style.css and scan route

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/scan')
+def scan():
+    return render_template('scan.html')
 
 
 if __name__ == '__main__':
