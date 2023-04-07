c43cab44a0a8304374e1336118934e641823ac5b
Dominic Kuncik <itsdomhd@gmail.com>
2023-04-07 14:20:43 +0100

    SQL Support
    Added in MariaDB Support
    Checked for first time user
    Added in Privacy Policy

---
+++ google_auth.py
@@ -0,0 +1,24 @@
+import json
+import os
+import pathlib
+
+from google_auth_oauthlib.flow import Flow
+
+
+def get_flow():
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    GOOGLE_CLIENT_ID = json.load(open("client_secret.json", "r"))["web"]["client_id"]
+    client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+    flow = Flow.from_client_secrets_file(
+        client_secrets_file=client_secrets_file,
+        scopes=[
+            "https://www.googleapis.com/auth/userinfo.email",
+            "openid",
+            "https://www.googleapis.com/auth/userinfo.profile"
+        ],
+        redirect_uri="http://127.0.0.1:5000/callback"
+    )
+
+    return flow, GOOGLE_CLIENT_ID
