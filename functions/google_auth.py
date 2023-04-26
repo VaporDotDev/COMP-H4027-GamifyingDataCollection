23d696b9526584dc85cca37af1871c63ad3727ae
Dominic Kuncik <itsdomhd@gmail.com>
2023-04-26 14:02:10 +0100

    CWD in google_auth.py

--- functions/google_auth.py
+++ functions/google_auth.py
@@ -8,11 +8,13 @@ from google_auth_oauthlib.flow import Flow
 def get_flow():
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+    cwd = os.getcwd()
+
     GOOGLE_CLIENT_ID = \
-    json.load(open(r"C:\Users\super\PycharmProjects\COMP-H4027-ProjectFlask\client_secret.json", "r"))["web"][
-        "client_id"]
+        json.load(open(os.path.join(cwd, "client_secret.json"), "r"))["web"][
+            "client_id"]
     client_secrets_file = os.path.join(pathlib.Path(__file__).parent,
-                                       r"C:\Users\super\PycharmProjects\COMP-H4027-ProjectFlask\client_secret.json")
+                                       os.path.join(cwd, "client_secret.json"))
 
     flow = Flow.from_client_secrets_file(
         client_secrets_file=client_secrets_file,
